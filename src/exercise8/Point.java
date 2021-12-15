afc7a3b028643213ac691daf5c3a84e6c026e225
AlexanderRuskov <>
2021-12-15 14:39:17 +0000

    Updated

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,34 +1,32 @@
 package exercise8;
-import java.util.Scanner;
 
 public class Point {
     // Define attributes
-       double x;
-       double y;
-    public Point() {
-        // Initialize attributes to 0
-       x = 0;
-       y= 0;
+    double x;
+    double y;
+
+
+    public Point() {// Initialize attributes to 0
+        x = 0;
+        y = 0;
     }
 
     public Point(double x, double y) {
         // Initialize attributes by coping x and y parameters
-          this.x = x;
-          this.y = y;
+        this.x = x;
+        this.y = y;
     }
 
     public Point(Point otherPoint) {
         // Initialize attributes by coping attributes of otherPoint
-         x = otherPoint.x;
-         y = otherPoint.y;
+        x = otherPoint.x;
+        y = otherPoint.y;
     }
 
     public void initialize() {
         // Change attributes through console
-        Scanner input = new Scanner(System.in);
-            x = input.nextDouble();
-            y = input.nextDouble();
-
+        x = Utils.INPUT.nextDouble();
+        y = Utils.INPUT.nextDouble();
     }
 
     public void translate(double xDelta, double yDelta) {
@@ -41,19 +39,18 @@ public class Point {
         // Create new point which is translated relative to this point
         double newX = x + xDelta;
         double newY = y + yDelta;
-        return new Point(newX, newY);
+        return new Point (newX, newY);
     }
 
     public boolean equals(Point otherPoint) {
-        // Compare attributes of this point to attributes of otherPoint for equality
+        // Compare attributes of this point to attributes of otherPoint for equality 
         boolean xEqual = Utils.equals(x, otherPoint.x);
         boolean yEqual = Utils.equals(y, otherPoint.y);
         return xEqual && yEqual;
-        
     }
 
     public String toString() {
         // Represent attributes as string
-         return "(" + x + ", " + y + ")";
+        return "(" + x + ", " + y + ")";
     }
-}
+      }
